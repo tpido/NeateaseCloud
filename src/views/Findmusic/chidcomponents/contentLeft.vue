dfe78e7d074917d45f0103f0bd199769ba382548
tpido <934559531@qq.com>
2022-04-12 18:28:24 +0800

    完成热门推荐

---
+++ src/views/Findmusic/chidcomponents/contentLeft.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="contentLeft">
+    <hot-recommend></hot-recommend>
+    <recman-playlist></recman-playlist>
+  </div>
+</template>
+
+<script>
+import hotRecommend from "./contentleftCP/hotrecommend.vue";
+import recmanPlaylist from "./contentleftCP/recmanplaylist.vue";
+export default {
+  name: "",
+  components: {
+    hotRecommend,
+    recmanPlaylist,
+  },
+};
+</script>
+<style scoped>
+#contentLeft {
+  padding: 4px 15px;
+  height: 1425px;
+  width: 729px;
+}
+</style>
